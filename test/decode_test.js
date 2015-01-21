d25dceb2efc27d9e7dd2fe7241e2a88bca78befe
toastynerd <tylermorgan86@gmail.com>
2015-01-21 10:00:14 -0800

    add better error handling and tests

--- test/decode_test.js
+++ test/decode_test.js
@@ -6,16 +6,26 @@ var csrft = require('../index');
 describe('encode and decode', function() {
   var token;
   before(function(done) {
-    csrft.encode({hello: 'world'}, 'testpassword', function(encrypted) {
+    csrft.encode({hello: 'world'}, 'testpassword', function(err, encrypted) {
       token = encrypted;
       done();
     });
   });
 
   it('should be able to decode with the same key', function(done) {
-    csrft.decode(token, 'testpassword', function(decoded) {
+    csrft.decode(token, 'testpassword', function(err, decoded) {
+      expect(err).to.eql(null);
       expect(decoded.hello).to.eql('world');
       done(); 
+    });
+  });
+
+  it('should not be able to decode with an invalid key', function(done) {
+    csrft.decode(token, 'wrongpassword', function(err, decoded) {
+      expect(err).to.not.eql(null);
+      expect(decoded).to.eql(undefined);
+      expect(err.message).to.eql('could not decode token');
+      done();
     });
   });
 });
--- test/encode_test.js
+++ test/encode_test.js
@@ -12,8 +12,18 @@ describe('encode a token', function() {
   });
 
   it('encode should return a string', function(done) {
-    csrft.encode({hello: "world"}, 'sometestsecret', function(token) {
+    csrft.encode({hello: "world"}, 'sometestsecret', function(err, token) {
+      expect(err).to.eql(null);
       expect(typeof(token)).to.eql('string');
+      done();
+    });
+  });
+
+  it('should not encode invaild JSON', function(done) {
+    csrft.encode("{hello: broken", 'sometestsecret', function(err, token) {
+      expect(err).to.not.eql(null);
+      expect(token).to.eql(undefined);
+      expect(err.message).to.eql('invalid JSON');
       done();
     });
   });
